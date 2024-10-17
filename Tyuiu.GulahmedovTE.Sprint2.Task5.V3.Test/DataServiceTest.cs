9bc3c74d3963142ae1d0b6b6952bafc9e6e2c87e
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-17 23:06:32 +0500

    Task5

---
+++ Tyuiu.GulahmedovTE.Sprint2.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GulahmedovTE.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint2.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFirstDayName()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            string res = ds.FindDayName(x);
+            string wait = "понедельник";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GulahmedovTE.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.GulahmedovTE.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Исходные данные                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Введите  порядковый номер дня недели: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((x < 1) || (x > 7))
+            {
+                res = "Неверно введен номер дня недели";
+            }
+            else
+            {
+                res = "Это " + ds.FindDayName(x);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+        }
+    }
+}
