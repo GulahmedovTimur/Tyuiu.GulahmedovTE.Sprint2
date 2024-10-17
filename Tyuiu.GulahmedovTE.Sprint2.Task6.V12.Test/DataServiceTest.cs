459d866ccc3139b8acfd0ecc6f3c93c6e984f13e
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-17 23:20:57 +0500

    Task6

--- Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test/DataServiceTest.cs
+++ Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -1,25 +1,2 @@
 using Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Lib;
 namespace Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test
-{
-    [TestClass]
-    public class DataServiceTest
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            DataService ds = new DataService();
-            Assert.AreEqual("Январь", ds.FindMonthName(1990, 1));
-            Assert.AreEqual("Февраль", ds.FindMonthName(1990, 2));
-            Assert.AreEqual("Март", ds.FindMonthName(1990, 3));
-            Assert.AreEqual("Апрель", ds.FindMonthName(1990, 4));
-            Assert.AreEqual("Май", ds.FindMonthName(1990, 5));
-            Assert.AreEqual("Июнь", ds.FindMonthName(1990, 6));
-            Assert.AreEqual("Июль", ds.FindMonthName(1990, 7));
-            Assert.AreEqual("Август", ds.FindMonthName(1990, 8));
-            Assert.AreEqual("Сентябрь", ds.FindMonthName(1990, 9));
-            Assert.AreEqual("Октябрь", ds.FindMonthName(1990, 10));
-            Assert.AreEqual("Ноябрь", ds.FindMonthName(1990, 11));
-            Assert.AreEqual("Декабрь", ds.FindMonthName(1990, 12));
-        }
-    }
-}
--- Tyuiu.GulahmedovTE.Sprint2.Task6.V12/Program.cs
+++ Tyuiu.GulahmedovTE.Sprint2.Task6.V12/Program.cs
@@ -1,28 +1,2 @@
 ﻿using Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Lib;
 namespace Tyuiu.GulahmedovTE.Sprint2.Task6.V12
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите значение переменной StartYear, n");
-            int startYear, n;
-            startYear = Convert.ToInt32(Console.ReadLine());
-            n = Convert.ToInt32(Console.ReadLine());
-            DataService ds = new DataService();
-
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("***************************************************************************");
-
-            Console.WriteLine("StartYear: " + startYear + " " + "n: " + n);
-
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
-            var res = ds.FindMonthName(startYear, n);
-            Console.WriteLine(res);
-            Console.ReadKey();
-        }
-    }
-}
