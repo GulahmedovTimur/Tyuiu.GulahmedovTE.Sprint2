793fe39f8d7e334c0083288dd04a2921948fedcb
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-17 23:35:39 +0500

    Task7

---
+++ Tyuiu.GulahmedovTE.Sprint2.Task7.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.GulahmedovTE.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.GulahmedovTE.Sprint2.Task7.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
