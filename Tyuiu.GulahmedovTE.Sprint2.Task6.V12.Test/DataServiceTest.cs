7a5bc21d9626049e9728cce405d0a723dcfcdb65
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-10-17 23:41:27 +0500

    Task6

--- Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test/DataServiceTest.cs
+++ Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -1,2 +1,23 @@
 using Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Lib;
 namespace Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+
+            int g = 23;
+            int m = 1;
+            int n = 1;
+
+            string res = ds.FindDateOfPreviousDay(g, m, n);
+            string w = "31.12.22";
+
+
+            Assert.AreEqual(Convert.ToString(w), res);
+        }
+    }
+}
--- Tyuiu.GulahmedovTE.Sprint2.Task6.V12/Program.cs
+++ Tyuiu.GulahmedovTE.Sprint2.Task6.V12/Program.cs
@@ -1,2 +1,42 @@
 ﻿using Tyuiu.GulahmedovTE.Sprint2.Task6.V12.Lib;
 namespace Tyuiu.GulahmedovTE.Sprint2.Task6.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int g, m, n;
+
+            Console.WriteLine("Введите значение G: ");
+            g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение M: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            string res = ds.FindDateOfPreviousDay(g, m, n);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (((m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && (n < 1 || n > 31)) || ((m == 4 || m == 6 || m == 9 || m == 11) && (n < 1 || n > 30)) || (m == 29 && (n < 1 || n > 29)) || (m < 1 || m > 12))
+            {
+                Console.WriteLine("Введено неверное значение!");
+            }
+            else
+            {
+                Console.WriteLine(res);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
